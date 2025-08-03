21ff2524c283d02cc187714c149554d155069085
kwadra <git@demosoft.org>
2025-08-02 20:41:15 -0700

    Add a reconnect if the mqtt connection is lost

--- src/SplitFlapDisplay.cpp
+++ src/SplitFlapDisplay.cpp
@@ -199,6 +199,10 @@ void SplitFlapDisplay::writeString(String inputString, float speed, bool centeri
 
     if (mqtt && mqtt->isConnected()) {
         mqtt->publishState(displayString);
+    } else if (mqtt) {
+        // reconnect to MQTT if not connected
+        mqtt->setup();
+
     }
 }
 
